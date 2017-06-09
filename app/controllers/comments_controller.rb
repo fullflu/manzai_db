b85d8d24bcda29cc9d444713222b7baff5ac2e8d
fullflu <k.takayama0902@gmail.com>
2017-06-09 14:06:39 +0900

    add edit to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,27 @@ class CommentsController < ApplicationController
         redirect_to controller: :products, action: :show, id: params[:product_id]
     end
 
+    def update
+        #binding.pry
+        comment = Comment.find(params[:id])
+        if comment.product.user_id == current_user.id
+            comment.update(update_params)
+        end
+        redirect_to controller: :products, action: :show, id: comment.product_id
+    end
+
+    def edit
+        @comment = Comment.find(params[:id])
+        if @comment.prev_id
+            @prev_comment = Comment.find(@comment.prev_id)
+        end
+        if @comment.post_id
+            @post_comment = Comment.find(@comment.post_id)
+        end
+        # binding.pry
+        #redirect_to controller: :products, action: :show, id: @comment.product_id
+    end
+
     def destroy
         comment = Comment.find(params[:id])
         if comment.product.user_id == current_user.id
@@ -51,5 +72,8 @@ class CommentsController < ApplicationController
         #binding.pry
         # params.require(:comment).permit(:daihon).merge(product_id: params[:product_id])
         params.require("comment").permit(:daihon).merge(product_id: params[:product_id],tsukkomi: params.require("comment")[:tsukkomi], prev_id: params.require("comment")[:prev_id])
+    end
+    def update_params
+        params.require("comment").permit(:daihon)
     end
 end
