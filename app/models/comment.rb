4c744631a7f2caf31a509a9397b2141bef0617f2
fullflu <k.takayama0902@gmail.com>
2017-06-16 14:50:02 +0900

    adjust blank input for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
        likes.find_by(user_id: user_id)
         # like_uid = Like.select(:user_id).where(comment_id: comment.id).pluck(:user_id)
     end
-    # validates :daihon, presence: true
+    validates :daihon, presence: true
     # validates :tsukkomi, presence: true
     # validates :tsukkomi, inclusion:{in: [true, false]}
 end
