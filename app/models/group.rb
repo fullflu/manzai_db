a145a03e7986b267244e3d217483aa3736d82b96
fullflu <k.takayama0902@gmail.com>
2017-06-20 10:29:54 +0900

    add dependent association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-    has_many :products
+    has_many :products, dependent: :destroy
     validates :name, presence: true, uniqueness: true
     validates :boke, presence: true
     validates :tsukkomi, presence: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
     belongs_to :user
     belongs_to :group
-    has_many :comments
+    has_many :comments, dependent: :destroy
     validates :title, presence: true
 end
